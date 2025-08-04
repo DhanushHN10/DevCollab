4a2b61fb599c798566a707515b4d1c3025b4cf66
DhanushHN10 <dhanush.hn01@gmail.com>
2025-08-04 23:21:36 +0530

    Google OAuth working

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -10,10 +10,17 @@ import { jwtDecode} from 'jwt-decode';
 
 
 
+
+
 export default function LoginPage() {
     const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  const handleGoogleLogin = () => {
+  window.location.href = 'http://localhost:5000/api/auth/oauth/google';
+};
+
 
 
 const handleLogin = async () => {
@@ -67,7 +74,14 @@ onClick={handleLogin}>
   Sign In
 </Button>
 
-<Button className="mt-6 w-full bg-white/20 hover:bg-white/70 hover:text-black border border-white text-white transition-transform duration-200 hover:scale-105">
+<Button className="mt-6 w-full bg-white/20 hover:bg-white/70 hover:text-black border border-white text-white transition-transform duration-200 hover:scale-105"
+onClick={handleGoogleLogin}
+>
+  <img
+  src="https://www.svgrepo.com/show/475656/google-color.svg"
+  alt="Google"
+  className="relative w-5 h-5 mr-2 z-50 bg-white/60  "
+/>
   <span className="font-semibold font-roboto">Sign In with Google</span>
 </Button>
 
@@ -75,10 +89,10 @@ onClick={handleLogin}>
 
           <p className="mt-4 text-center text-sm text-white/70">
             Don't have an account?
-            <br/>
+            <br/>  </p>
             <div className=" pt-2 transition-transform duration-200 hover:scale-110 hover:text-white/30">
-            <a href="#" ><Link to='/signup'>SignUp</Link></a></div>
-          </p>
+            <Link to='/signup'>SignUp</Link></div>
+          {/* </p> */}
         </div>
       </div>
     </div>
