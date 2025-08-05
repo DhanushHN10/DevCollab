dccc5366b2b1e3bd9a403206574a41f8b59f71a8
DhanushHN10 <dhanush.hn01@gmail.com>
2025-08-05 20:14:37 +0530

    Prod ready

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-    origin:'http://localhost:5173'
+    origin: process.env.FRONTEND_URI || 'http://localhost:5173'
 
 }))
 app.use(passport.initialize());
