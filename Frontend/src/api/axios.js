0a155d731b471c6f860935a925acddba5502c481
DhanushHN10 <dhanush.hn01@gmail.com>
2025-08-05 20:14:57 +0530

    production ready

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000', // or your backend base URL
+  baseURL: import.meta.env.VITE_API_BASE_URL, 
 //   withCredentials: true, 
 });
 
