eb6135cb6002c5af81785382c39b66b81087d18b
DhanushHN10 <dhanush.hn01@gmail.com>
2026-05-16 00:11:19 +0530

    Settin up Notification Service with persistent storage.

---
+++ Backend/models/Notifications.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const NotificationsSchema = new mongoose.Schema({
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  message: { type: String, required: true },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Notification = mongoose.model(
+  "Notification",
+  NotificationsSchema,
+);
+
+export default Notification;
