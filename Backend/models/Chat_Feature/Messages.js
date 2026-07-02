7e9a77b749e4fa3fec2a38ac09898d84a9707a39
DhanushHN10 <dhanush.hn01@gmail.com>
2026-07-03 00:24:59 +0530

    Added idempocy check through a frontend generated key to avoid duplicate stores of the same message.
    2) Added timestamps.

--- Backend/models/Chat_Feature/Messages.js
+++ Backend/models/Chat_Feature/Messages.js
@@ -1,40 +1,56 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-
-    conversationId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Conversation',
-        required: true,
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
     },
 
-    senderId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
 
-    recipientId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+    recipientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      // required: true
     },
 
-    content:{
+    content: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+
+    readBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    // For Idempocy if the FE sends the same message multiple times due to network issues, we can use this field to identify and ignore multiple sends.
+    clientMessageId:{
         type: String,
-        required: true,
-    },
+    } 
+  },
+  { timestamps: true },
 
-    createdAt:{
-        type: Date,
-        default : Date.now
-    }
-});
+);
 
 messageSchema.index({
-    conversationId: 1,
-    createdAt: -1 // So that the most recent messages come first when querying by conversationId
-})
+  conversationId: 1,
+  createdAt: -1, // So that the most recent messages come first when querying by conversationId
+});
 
-const Message = mongoose.model('Message', messageSchema);
+
+const Message = mongoose.model("Message", messageSchema);
 export default Message;
